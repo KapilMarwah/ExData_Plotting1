7fcea0af024ece02c132f720de035461e92da09c
KapilMarwah <marwah.kapil@gmail.com>
2019-04-19 15:21:53 +0530

    Week1 Assignment Update

--- ExData_Plotting1/plot1.R
+++ ExData_Plotting1/plot1.R
@@ -14,6 +14,7 @@ household <- read.table("./household_power_consumption.txt", header = TRUE, sep 
 h1 <- household[household$Date %in% c("1/2/2007", "2/2/2007"),]
 
 png(filename = "plot1.png", width = 480, height = 480)
+h1$Global_active_power <- as.numeric(h1$Global_active_power)
 hist(h1$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", 
      main = "Global Active Power")
 dev.off()
